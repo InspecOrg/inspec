604141d9b2c58a1de915c05fc980eb6ddb88f9c1
Gustavo Sabino <gustavo.sabino@hotmail.com>
2016-05-06 02:59:46 -0300

    Added modal for sign up

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,7 +4,25 @@ angular.module('inspec.controllers', [])
 
 .controller('SearchCtrl', function($scope) {})
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', function($scope, $ionicModal) {
+  $ionicModal.fromTemplateUrl('templates/modal-signup.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
 })
 
 .controller('AboutCtrl', function($scope) {})
